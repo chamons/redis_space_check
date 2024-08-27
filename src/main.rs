233aa8dd408e64a67575d4e0e18bf42310ddb985
Chris Hamons <chrishamons@onesignal.com>
2024-08-27 10:47:15 -0500

    dev: Parallelize

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,20 @@ use std::fmt::Write;
 
 use eyre::Result;
 use indicatif::{ProgressBar, ProgressState, ProgressStyle};
-use itertools::Itertools;
+use r2d2_redis::{r2d2, redis};
 use rand::Rng;
+use rayon::prelude::*;
 
 const ITEM_COUNT: u64 = 10_000_000;
 
 fn main() -> Result<()> {
-    let client = redis::Client::open("redis://localhost:6379")?;
-    let mut con = client.get_connection()?;
+    let manager = r2d2_redis::RedisConnectionManager::new("redis://localhost:6379")?;
+    let pool = r2d2::Pool::builder().max_size(64).build(manager)?;
 
-    redis::cmd("FLUSHDB").execute(&mut con);
+    {
+        let mut con = pool.get().unwrap();
+        redis::cmd("FLUSHDB").execute(&mut *con);
+    }
 
     let pb = ProgressBar::new(ITEM_COUNT);
     pb.set_style(
@@ -27,22 +31,17 @@ fn main() -> Result<()> {
         }),
     );
 
-    for chunk in (0..ITEM_COUNT).chunks(100).into_iter() {
-        let mut pipe = redis::pipe();
-
-        for _ in chunk {
-            pipe.cmd("SET")
-                .arg(uuid::Uuid::new_v4().as_bytes().to_vec())
-                .arg(rand::thread_rng().gen::<i64>())
-                .arg("EX")
-                .arg(1024)
-                .arg("NX")
-                .execute(&mut con);
-
-            pb.inc(1);
-        }
-        pipe.execute(&mut con);
-    }
+    (0..ITEM_COUNT).into_par_iter().for_each(|_| {
+        let mut con = pool.get().unwrap();
+        redis::cmd("SET")
+            .arg(uuid::Uuid::new_v4().as_bytes().to_vec())
+            .arg(rand::thread_rng().gen::<i64>())
+            .arg("EX")
+            .arg(1024)
+            .arg("NX")
+            .execute(&mut *con);
+        pb.inc(1);
+    });
 
     Ok(())
 }
